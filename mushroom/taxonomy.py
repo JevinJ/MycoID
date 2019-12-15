2399549eb224d1112772c147e74bd199f84275ad
JevinJ <jevinjones@openmailbox.org>
2019-12-15 14:31:09 -0800

    Add taxonomy.py outline.

---
+++ mushroom/taxonomy.py
@@ -0,0 +1,13 @@
+from .base import Base
+from sqlalchemy import Column, Integer, ForeignKey, String
+
+
+class Taxonomy(Base):
+    id = Column(Integer, primary_key=True)
+    mushroom_id = Column(Integer, ForeignKey('mushrooms.id'))
+    common_names = Column(String)
+    name_origin = Column(String)
+    order = Column(String)
+    family = Column(String)
+    genus = Column(String)
+    species = Column(String)
