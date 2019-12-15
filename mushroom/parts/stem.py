093a1ab117ccf4a4b57ee083ea177e8684edaf0c
JevinJ <jevinjones@openmailbox.org>
2019-12-15 00:09:12 -0800

    Add stem.py outline.

---
+++ mushroom/parts/stem.py
@@ -0,0 +1,13 @@
+from ..base import Base
+from sqlalchemy import Column, Integer, Float
+
+
+class Stem(Base):
+    """Description of a mushrooms' stem."""
+    __tablename__ = 'Stem'
+    id = Column(Integer, primary_key=True)
+    min_height = Column(Float)
+    max_height = Column(Float)
+    min_width = Column(Float)
+    max_width = Column(Float)
+
