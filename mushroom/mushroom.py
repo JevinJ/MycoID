6e0c50fbf73c9f5eefd8b11c9d819af0ddb5eabe
JevinJ <jevinjones@openmailbox.org>
2019-12-15 14:18:06 -0800

    Add mushroom.py

--- mushroom/mushroom.py
+++ mushroom/mushroom.py
@@ -1,2 +1,16 @@
-class Mushroom:
-    __slots__ = ('')
+from .base import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+
+class Mushroom(Base):
+    """Information/data in common with all mushrooms."""
+    __tablename__ = 'mushrooms'
+    id = Column(Integer, primary_key=True)
+    description = Column(String)
+    habitat = relationship('Habitat', uselist=False, backref='mushrooms')
+    smell = relationship('Smell', uselist=False, backref='mushrooms')
+    spores = relationship('Spore', uselist=False, backref='mushrooms')
+    taste = relationship('Taste', uselist=False, backref='mushrooms')
+    taxonomy = relationship('Taxonomy', uselist=False, backref='mushrooms')
+    wikipedia_url = Column(String)
