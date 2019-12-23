22d4067a388754705f558e73515d0b90307cc322
JevinJ <jevinjones@openmailbox.org>
2019-12-22 21:41:09 -0800

    Add database creation.

--- mycomatch.py
+++ mycomatch.py
@@ -3,4 +3,10 @@ class MycoMatch:
 
 
 if __name__ == '__main__':
+    from mushroom import *
+    from mushroom.base import Base
+    from sqlalchemy import create_engine
+
+    engine = create_engine('sqlite:///mycomatch.db')
+    Base.metadata.create_all(engine)
     app = MycoMatch()
