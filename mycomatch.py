5122637cc40b0c28c64fec34b34c9ab1d0605fd8
JevinJ <jevinjones@openmailbox.org>
2019-12-22 23:54:20 -0800

    Add session creation.

--- mycomatch.py
+++ mycomatch.py
@@ -6,7 +6,9 @@ if __name__ == '__main__':
     from mushroom import *
     from mushroom.base import Base
     from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
 
     engine = create_engine('sqlite:///mycomatch.db')
-    Base.metadata.create_all(engine)
+    Base.metadata.create_all(bind=engine)
+    session = sessionmaker(bind=engine)
     app = MycoMatch()
