523d67129dc81587340c3330f9c3346cb61d3767
JevinJ <jevinjones@openmailbox.org>
2019-12-26 20:32:52 -0800

    Add TagMapping to tag_table.py

--- mushroom/tag_table.py
+++ mushroom/tag_table.py
@@ -4,3 +4,7 @@ from sqlalchemy import Column, Integer, String
 class TagTable:
     id = Column(Integer, primary_key=True)
     title = Column(String)
+
+
+class TagMapping:
+    tag_id = Column(Integer, primary_key=True)
