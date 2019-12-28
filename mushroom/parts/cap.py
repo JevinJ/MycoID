efaff39c26a19d3695a8effde4b9513142b26f21
JevinJ <jevinjones@openmailbox.org>
2019-12-27 18:02:47 -0800

    Add precision to Float columns and length to color column.

--- mushroom/parts/cap.py
+++ mushroom/parts/cap.py
@@ -6,7 +6,7 @@ class Cap(Base):
     """Description of a mushrooms' cap."""
     __tablename__ = 'caps'
     fungi_id = Column(Integer, ForeignKey('fungi.id'), primary_key=True)
-    min_diameter = Column(Float)
-    max_diameter = Column(Float)
-    color = Column(String)
+    min_diameter = Column(Float(precision=2))
+    max_diameter = Column(Float(precision=2))
+    color = Column(String(length=7))
     shape = Column(String)
