5dc2c7e3728795f468965b9010b38cec06dad751
JevinJ <jevinjones@openmailbox.org>
2019-12-29 00:45:05 -0800

    Add mixins.py and HasReportConsensus mixin.

---
+++ mixins.py
@@ -0,0 +1,10 @@
+import sqlalchemy as db
+
+
+class HasReportConsensus:
+    """When a descriptive item on a fungus is tagged with a value,
+     this value is incremented, when removed, decremented. This allows
+     for sorting to determine the most common traits, as well as removing false traits.
+     For example, if a fungus is tagged as associating with pine trees more often than oak,
+     the consensus for pine would be higher."""
+    report_consensus = db.Column(db.Integer, default=1, nullable=False)
