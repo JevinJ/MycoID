33267a000f4ce4e2b3525428b7d25a316741c3a2
JevinJ <jevinjones@openmailbox.org>
2019-12-28 20:33:25 -0800

    Remove TagMapping

--- mushroom/tagging.py
+++ mushroom/tagging.py
@@ -1,16 +1,6 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy import Column, Integer, String
 
 
 class TagTable:
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String)
-
-
-class TagMapping:
-    """Maps a fungus table to a tag table."""
-    @declared_attr
-    def fungi_id(self):
-        return Column(Integer, ForeignKey('fungi.id'), primary_key=True)
-    tag_id = Column(Integer, primary_key=True)
-    times_reported = Column(Integer, nullable=False)
