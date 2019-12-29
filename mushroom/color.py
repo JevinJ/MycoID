5170561ad21ac9e9375b9e2ec1c6c3f6486d21c7
JevinJ <jevinjones@openmailbox.org>
2019-12-29 00:48:40 -0800

    Make hex_value 6 length instead of 7, don't need to store '#'.

--- mushroom/color.py
+++ mushroom/color.py
@@ -6,4 +6,4 @@ class Color(Base):
     __tablename__ = 'colors'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32))
-    hex_value = db.Column(db.String(7))
+    hex_value = db.Column(db.String(6))
