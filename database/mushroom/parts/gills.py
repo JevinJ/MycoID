32bcedcbd428f1359474ddffceb729ff632ba7b8
JevinJ <jevinjones@openmailbox.org>
2019-12-30 00:21:03 -0800

    Add GillColor

--- database/mushroom/parts/gills.py
+++ database/mushroom/parts/gills.py
@@ -1,5 +1,7 @@
+from ...mixins import HasReportConsensus
 from database.db_base import Base
 from sqlalchemy import Column, ForeignKey, String, Integer, Enum
+from sqlalchemy.orm import relationship
 
 
 class Gills(Base):
@@ -8,5 +10,11 @@ class Gills(Base):
     fungi_id = Column(Integer, ForeignKey('fungi.id'), primary_key=True)
     attachment = Column(Enum)
     closeness = Column(Enum)
-    color = Column(String)
+    color = relationship('GillColor')
     forking = Column(String)
+
+
+class GillColor(Base, HasReportConsensus):
+    __tablename__ = 'gill_colors'
+    fungi_id = Column(Integer, ForeignKey('gills.fungi_id'), primary_key=True)
+    color_id = Column(Integer, ForeignKey('colors.id'), primary_key=True)
