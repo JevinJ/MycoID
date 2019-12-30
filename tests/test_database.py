4bf19444f182bc5352fad0186e9998fcf3cd705a
JevinJ <jevinjones@openmailbox.org>
2019-12-29 22:11:12 -0800

    Add test_database.py

---
+++ tests/test_database.py
@@ -0,0 +1,22 @@
+from database.mushroom import *
+from database.db_base import Base
+from pytest import fixture
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+@fixture(scope='module')
+def db_engine():
+    engine = create_engine('sqlite:///:memory:')
+    yield engine
+
+
+@fixture(scope='module')
+def db_session(db_engine):
+    session = sessionmaker(bind=db_engine)()
+    yield session
+    session.close()
+
+
+def test_should_create_all(db_engine):
+    Base.metadata.create_all(bind=db_engine)
