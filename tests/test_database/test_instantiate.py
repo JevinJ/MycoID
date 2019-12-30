4fed510a03ced1c3fa0c91d6c731c9ea09a5cad2
JevinJ <jevinjones@openmailbox.org>
2019-12-30 02:09:18 -0800

    Add test_should_instantiate_all_orms()

--- tests/test_database/test_instantiate.py
+++ tests/test_database/test_instantiate.py
@@ -1,6 +1,22 @@
 from database.mushroom import *
 from database.db_base import Base
+import pytest
+from sqlalchemy.ext.declarative import DeclarativeMeta
+import inspect
+import sys
 
 
 def test_should_create_all(db_engine):
     Base.metadata.create_all(bind=db_engine)
+
+
+all_orms = []
+for _, obj in inspect.getmembers(sys.modules[__name__]):
+    if inspect.isclass(obj) and isinstance(obj, DeclarativeMeta):
+        all_orms.append(obj)
+
+
+@pytest.mark.parametrize('orm_class', all_orms)
+def test_should_instantiate_all_orms(orm_class):
+    orm_class()
+
