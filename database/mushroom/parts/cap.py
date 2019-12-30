93aae5e5173af55a2208481d75b3eb2e57ebc30e
JevinJ <jevinjones@openmailbox.org>
2019-12-30 00:17:57 -0800

    Add CapColor

--- database/mushroom/parts/cap.py
+++ database/mushroom/parts/cap.py
@@ -1,4 +1,4 @@
-from ...mixins import HasWidth
+from ...mixins import HasWidth, HasReportConsensus
 from sqlalchemy import Column, Integer, ForeignKey, String
 from sqlalchemy.orm import relationship
 from database.db_base import Base
@@ -9,7 +9,7 @@ class Cap(Base):
     __tablename__ = 'caps'
     fungi_id = Column(Integer, ForeignKey('fungi.id'), primary_key=True)
     diameters = relationship('CapDimensions')
-    color = Column(String(length=7))
+    colors = relationship('CapColor')
     shape = Column(String)
 
 
@@ -17,3 +17,9 @@ class CapDimensions(Base, HasWidth):
     __tablename__ = 'cap_dimensions'
     id = Column(Integer, primary_key=True)
     fungi_id = Column(Integer, ForeignKey('caps.fungi_id'), primary_key=True)
+
+
+class CapColor(Base, HasReportConsensus):
+    __tablename__ = 'cap_colors'
+    fungi_id = Column(Integer, ForeignKey('caps.fungi_id'), primary_key=True)
+    color_id = Column(Integer, ForeignKey('colors.id'), primary_key=True)
