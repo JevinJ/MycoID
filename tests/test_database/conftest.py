64b4190cd18e80c013f227aa3cfdba189b51755b
JevinJ <jevinjones@openmailbox.org>
2019-12-30 23:40:05 -0800

    Reimplement db_connection(), db_session()

--- tests/test_database/conftest.py
+++ tests/test_database/conftest.py
@@ -1,15 +1,25 @@
+from database.db_base import BaseModel
+from database.mappings import *
 from pytest import fixture
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-@fixture
-def db_engine():
-    return create_engine('sqlite:///:memory:')
+engine = create_engine('sqlite:///:memory:')
+BaseModel.metadata.create_all(bind=engine)
+
+
+@fixture(scope='module')
+def db_connection():
+    connection = engine.connect()
+    yield connection
+    connection.close()
 
 
 @fixture
-def db_session(db_engine):
-    session = sessionmaker(bind=db_engine)()
+def db_session(db_connection):
+    transaction = db_connection.begin()
+    session = sessionmaker()(bind=db_connection)
     yield session
     session.close()
+    transaction.rollback()
