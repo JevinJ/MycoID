40b598b16a80fa0bea9bc2cf6db414413f8f54e9
JevinJ <jevinjones@openmailbox.org>
2019-12-31 19:18:47 -0800

    Finish Edibility.type column

--- database/mappings/edibility.py
+++ database/mappings/edibility.py
@@ -1,9 +1,10 @@
 from database.db_base import BaseModel
 from sqlalchemy import Column, Enum, Integer, ForeignKey, String
+from ..enums import EdibilityType
 
 
 class Edibility(BaseModel):
     __tablename__ = 'edibility'
     fungi_id = Column(Integer, ForeignKey('fungi.id'), primary_key=True)
-    edibility = Column(Enum)
+    type = Column(Enum(EdibilityType))
     notes = Column(String)
