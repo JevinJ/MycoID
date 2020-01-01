e5fb85642222349606a08affee86515b19c4850b
JevinJ <jevinjones@openmailbox.org>
2019-12-31 20:06:11 -0800

    Add GillAttachment, GillSpacing tables.

--- database/mappings/mushroom_parts/gills.py
+++ database/mappings/mushroom_parts/gills.py
@@ -1,5 +1,6 @@
 from ...mixins import HasReportConsensus
 from database.db_base import BaseModel
+from database.enums import GillAttachmentType, GillSpacingType
 from sqlalchemy import Column, ForeignKey, String, Integer, Enum
 from sqlalchemy.orm import relationship
 
@@ -8,8 +9,8 @@ class Gills(BaseModel):
     """Description of a mushrooms' gills."""
     __tablename__ = 'gills'
     fungi_id = Column(Integer, ForeignKey('fungi.id'), primary_key=True)
-    attachment = Column(Enum)
-    closeness = Column(Enum)
+    attachment = relationship('GillAttachment')
+    closeness = relationship('GillSpacing')
     color = relationship('GillColor')
     forking = Column(String)
 
@@ -18,3 +19,15 @@ class GillColor(BaseModel, HasReportConsensus):
     __tablename__ = 'gill_colors'
     fungi_id = Column(Integer, ForeignKey('gills.fungi_id'), primary_key=True)
     color_id = Column(Integer, ForeignKey('colors.id'), primary_key=True)
+
+
+class GillAttachment(BaseModel, HasReportConsensus):
+    __tablename__ = 'gill_attachment'
+    fungi_id = Column(Integer, ForeignKey('gills.fungi_id'), primary_key=True)
+    value = Column(Enum(GillAttachmentType))
+
+
+class GillSpacing(BaseModel, HasReportConsensus):
+    __tablename__ = 'gill_spacing'
+    fungi_id = Column(Integer, ForeignKey('gills.fungi_id'), primary_key=True)
+    value = Column(Enum(GillSpacingType))
