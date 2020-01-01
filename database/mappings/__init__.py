f9d881b83ec4e6345147f7530abd598fd179fad2
JevinJ <jevinjones@openmailbox.org>
2019-12-31 18:49:55 -0800

    Switch from direct to * import in __init__.py, should be easier to maintain and everything here is a necessary mapping.

--- database/mappings/__init__.py
+++ database/mappings/__init__.py
@@ -1,15 +1,14 @@
-from .color import Color
-from .description import Description
-from .ecology import ClusteringHabits, Ecology, EcologyTypes, MycorrhizalHost, ParasiticHost, \
-                     SaprobicSubstrate, FungiMycorrhizalHost, FungiParasiticHost, FungiSaprobicSubstrate
-from .edibility import Edibility
-from .fungus import Fungus
-from .mushroom_parts.cap import Cap, CapColor, CapDimensions
-from .mushroom_parts.gills import GillColor, Gills
-from .mushroom_parts.stem import Stem, StemColor, StemDimensions
-from .mushroom_parts.veil import PartialVeil, UniversalVeil
-from .smell import Smell
-from .spores import Spores, SporeColor, SporeDimensions
-from .taste import Taste
-from .taxon import Taxon
-from .wikipedia_url import WikipediaUrl
+from .color import *
+from .description import *
+from .ecology import *
+from .edibility import *
+from .fungus import *
+from .mushroom_parts.cap import *
+from .mushroom_parts.gills import *
+from .mushroom_parts.stem import *
+from .mushroom_parts.veil import *
+from .smell import *
+from .spores import *
+from .taste import *
+from .taxon import *
+from .wikipedia_url import *
