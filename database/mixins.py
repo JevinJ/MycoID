ed1d655010608c473c4fbb0363dbcb33b405bc99
JevinJ <jevinjones@openmailbox.org>
2020-01-01 18:54:24 -0800

    Add TagTable.description column

--- database/mixins.py
+++ database/mixins.py
@@ -59,5 +59,6 @@ class TagTable:
     id = Column(Integer, primary_key=True)
     type = Column(String)
     name = Column(String(64))
+    description = Column(String)
 
     __mapper_args___ = {'polymorphic_on': type}
