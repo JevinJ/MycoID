0de16657a2ec7b9aa4ea048b4a671f4c42a999c4
JevinJ <jevinjones@openmailbox.org>
2020-01-01 20:42:12 -0800

    Directly import Column, Integer, String classes.

--- database/mappings/color.py
+++ database/mappings/color.py
@@ -1,10 +1,10 @@
 from database.db_base import BaseModel
-import sqlalchemy as db
+from sqlalchemy import Column, Integer, String
 from sqlalchemy_utils.types.color import ColorType
 
 
 class Color(BaseModel):
     __tablename__ = 'colors'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(32))
-    value = db.Column(ColorType)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(32))
+    value = Column(ColorType)
