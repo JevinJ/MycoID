14ae9fffb45bc16f2ee61e8aa02ff5b0b1c9842d
JevinJ <jevinjones@openmailbox.org>
2020-01-02 01:25:02 -0800

    Remove EdibilityType enum

--- database/enums.py
+++ database/enums.py
@@ -1,19 +1,6 @@
 from enum import Enum, auto
 
 
-class EdibilityType(Enum):
-    choice = auto()
-    edible = auto()
-    inedible = auto()
-    unpalatable = auto()
-    caution = auto()
-    psychoactive = auto()
-    poisonous = auto()
-    allergenic = auto()
-    deadly = auto()
-    unknown = auto()
-
-
 class CapShape(Enum):
     campanulate = auto()
     conical = auto()
