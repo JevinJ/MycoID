f59e50e35aba7e61cb00db80ef52adc04bbab38c
JevinJ <jevinjones@openmailbox.org>
2020-01-01 20:41:39 -0800

    Switch Color.hex_value to ColorType

--- database/mappings/color.py
+++ database/mappings/color.py
@@ -1,9 +1,10 @@
 from database.db_base import BaseModel
 import sqlalchemy as db
+from sqlalchemy_utils.types.color import ColorType
 
 
 class Color(BaseModel):
     __tablename__ = 'colors'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32))
-    hex_value = db.Column(db.String(7))
+    value = db.Column(ColorType)
