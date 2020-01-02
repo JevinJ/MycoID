9bf28163adc64f95b6236fc872febf265d3c9b10
JevinJ <jevinjones@openmailbox.org>
2020-01-01 20:40:23 -0800

    Switch WikipediaUrl.value from String to URLType.

--- database/mappings/wikipedia_url.py
+++ database/mappings/wikipedia_url.py
@@ -1,8 +1,9 @@
 from database.db_base import BaseModel
-from sqlalchemy import Column, Integer, ForeignKey, String
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy_utils.types.url import URLType
 
 
 class WikipediaUrl(BaseModel):
     __tablename__ = 'wikipedia_urls'
     fungi_id = Column(Integer, ForeignKey('fungi.id'), primary_key=True)
-    value = Column(String)
+    value = Column(URLType)
