89d10c7ac16cfc414e0a8c02d962260ae56ddf62
JevinJ <jevinjones@openmailbox.org>
2020-01-01 18:53:13 -0800

    Add TagTable.type and polymorphic_on argument.

--- database/mixins.py
+++ database/mixins.py
@@ -57,4 +57,7 @@ class HasLength:
 
 class TagTable:
     id = Column(Integer, primary_key=True)
+    type = Column(String)
     name = Column(String(64))
+
+    __mapper_args___ = {'polymorphic_on': type}
