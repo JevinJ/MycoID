c4e392fd24db7b69390c127a26afdf7bec5dc778
JevinJ <jevinjones@openmailbox.org>
2020-01-02 00:46:24 -0800

    Remove unused enums.

--- database/enums.py
+++ database/enums.py
@@ -1,19 +1,6 @@
 from enum import Enum, auto
 
 
-class EcologyType(Enum):
-    mycorrhizal = auto()
-    saprobic = auto()
-    parasitic = auto()
-
-
-class ClusteringHabit(Enum):
-    singular = auto()
-    gregarious = auto()
-    troop = auto()
-    caespitose = auto()
-
-
 class EdibilityType(Enum):
     choice = auto()
     edible = auto()
