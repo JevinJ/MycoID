4d34a3849924a09b007368773267b5d8c46be6a2
JevinJ <jevinjones@openmailbox.org>
2020-01-01 18:56:04 -0800

    Move TagTable to tag.py

--- database/mappings/tag.py
+++ database/mappings/tag.py
@@ -1,4 +1,8 @@
-class TagTable:
+from database.db_base import BaseModel
+from sqlalchemy import Column, Integer, String
+
+
+class TagTable(BaseModel):
     id = Column(Integer, primary_key=True)
     type = Column(String)
     name = Column(String(64))
