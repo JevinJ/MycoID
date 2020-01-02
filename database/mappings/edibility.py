fabadf2f47670d8295f7630ada676836a3384339
JevinJ <jevinjones@openmailbox.org>
2020-01-02 01:24:44 -0800

    Implement tagging for Edibility

--- database/mappings/edibility.py
+++ database/mappings/edibility.py
@@ -1,9 +1,19 @@
 from database.db_base import BaseModel
-from sqlalchemy import Column, Enum, Integer, ForeignKey
-from ..enums import EdibilityType
+from database.mappings import Tag
+from database.mixins import HasTag
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Edibility(BaseModel):
     __tablename__ = 'edibility'
     fungi_id = Column(Integer, ForeignKey('fungi.id'), primary_key=True)
-    type = Column(Enum(EdibilityType))
+    type = relationship('EdibilityType')
+
+
+class FungiEdibilityType(BaseModel, HasTag):
+    fungi_id = Column(Integer, ForeignKey('edibility.fungi_id'), primary_key=True)
+
+
+class EdibilityType(Tag):
+    __mapper_args__ = {'polymorphic_identity': 'edibility'}
