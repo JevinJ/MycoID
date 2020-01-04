1e102fe6f65db8a579e5a77f2d9676755ca2fb33
JevinJ <jevinjones@openmailbox.org>
2020-01-03 19:54:28 -0800

    Use new tagging classes. Remove original Edibility, no longer necessary.

--- database/mappings/edibility.py
+++ database/mappings/edibility.py
@@ -1,20 +1,11 @@
-from database.db_base import BaseModel
-from database.mappings import Tag
-from database.mixins import HasTagId
+from database.mappings.tag import FungusTagMapping,  Tag
 from sqlalchemy import Column, Integer, ForeignKey
-from sqlalchemy.orm import relationship
 
 
-class Edibility(BaseModel):
-    __tablename__ = 'edibility'
-    fungi_id = Column(Integer, ForeignKey('fungi.id'), primary_key=True)
-    type = relationship('FungiEdibilityType')
-
-
-class FungiEdibilityType(BaseModel, HasTagId):
-    __tablename__ = 'fungi_edibility_types'
+class FungusEdibilityType(FungusTagMapping):
+    __tablename__ = 'fungus_edibility_type'
     fungi_id = Column(Integer, ForeignKey('edibility.fungi_id'), primary_key=True)
 
 
-class EdibilityType(Tag):
+class Edibility(Tag):
     pass
