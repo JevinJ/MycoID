9fef379a7631b3ee9e96ed6e6c0e5ac9672f550d
JevinJ <jevinjones@openmailbox.org>
2020-01-03 17:25:31 -0800

    Add FungusTagMapping base class.

--- database/mappings/tag.py
+++ database/mappings/tag.py
@@ -1,5 +1,6 @@
 from database.db_base import BaseModel
-from sqlalchemy import Column, Integer, String
+from database.mixins import HasReportConsensus
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declared_attr
 
 
@@ -15,3 +16,22 @@ class Tag(BaseModel):
         if self.__name__ == 'Tag':
             return {'polymorphic_on': self.type}
         return {'polymorphic_identity': self.__name__}
+
+
+class FungusTagMapping(BaseModel, HasReportConsensus):
+    __abstract__ = True
+
+    @declared_attr
+    def fungi_id(self):
+        return Column(Integer, ForeignKey('fungi.id'), primary_key=True)
+
+    @declared_attr
+    def tag_id(self):
+        return Column(Integer, ForeignKey('tag.id'), primary_key=True)
+
+    @staticmethod
+    def new_mapping(mapping_name, table_name, fungi_id_column: str):
+        return type(mapping_name, (FungusTagMapping,), {
+            '__tablename__': table_name,
+            'fungi_id': Column(Integer, ForeignKey(fungi_id_column), primary_key=True)
+        })
