bd85f6f76cb777bf34529af476a1dcddb810e25c
JevinJ <jevinjones@openmailbox.org>
2020-01-05 02:25:39 -0800

    Add test_should_insert_quantity_floats()

--- tests/test_database/test_quantity_type.py
+++ tests/test_database/test_quantity_type.py
@@ -1,7 +1,7 @@
 from database.db_base import BaseModel, Session
 from database.types import QuantityType
 from hypothesis import given, settings
-from hypothesis.strategies import integers
+from hypothesis.strategies import integers, floats
 from sqlalchemy import Column, Integer
 from unit_registry import ureg
 import pytest
@@ -16,6 +16,19 @@ class QuantityExample(BaseModel):
 @pytest.mark.parametrize('quantity_type', [ureg.meter, ureg.millimeter, ureg.kilometer])
 @settings(max_examples=5)
 def test_should_insert_quantity(engine, scalar_quantity, quantity_type):
+    session = Session(bind=engine)
+    quantity_to_insert = scalar_quantity * quantity_type
+    quantity_example = QuantityExample(quantity=quantity_to_insert)
+    session.add(quantity_example)
+    session.commit()
+    result = session.query(QuantityExample).order_by(QuantityExample.id.desc()).first()
+    assert result.quantity == quantity_to_insert
+
+
+@given(scalar_quantity=floats(allow_infinity=False))
+@pytest.mark.parametrize('quantity_type', [ureg.meter, ureg.millimeter, ureg.kilometer])
+@settings(max_examples=5)
+def test_should_insert_quantity_floats(engine, scalar_quantity, quantity_type):
     session = Session(bind=engine)
     quantity_to_insert = scalar_quantity * quantity_type
     quantity_example = QuantityExample(quantity=quantity_to_insert)
