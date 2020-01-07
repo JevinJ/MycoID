cd6f1e31a78ddb81e1b7b43a370eb61f496a046c
JevinJ <jevinjones@openmailbox.org>
2020-01-06 22:10:10 -0800

    Combine test_should_insert_quantity_floats(), test_should_insert_quantity(). Update max_examples to 400. Set deadline to None as test sometimes take much longer then default.

--- tests/test_database/test_quantity_type.py
+++ tests/test_database/test_quantity_type.py
@@ -1,10 +1,9 @@
 from database.db_base import BaseModel, Session
 from database.types import QuantityType
 from hypothesis import given, settings
-from hypothesis.strategies import integers, floats
+from hypothesis.strategies import integers, floats, one_of
 from sqlalchemy import Column, Integer
 from unit_registry import ureg
-import pytest
 
 
 class QuantityExample(BaseModel):
@@ -12,29 +11,18 @@ class QuantityExample(BaseModel):
     quantity = Column(QuantityType(unit_registry=ureg))
 
 
-@given(scalar_quantity=integers())
-@pytest.mark.parametrize('quantity_type', [ureg.meter, ureg.millimeter, ureg.kilometer])
-@settings(max_examples=5)
-def test_should_insert_quantity(engine, scalar_quantity, quantity_type):
+@given(scalar_quantity=one_of(integers(), floats(allow_infinity=False, allow_nan=False)))
+@settings(deadline=None, max_examples=400)
+def test_should_insert_quantity(engine, scalar_quantity):
     session = Session(bind=engine)
-    quantity_to_insert = scalar_quantity * quantity_type
+    session.query(QuantityExample).delete()
+    session.commit()
+
+    quantity_to_insert = scalar_quantity * ureg.millimeter
     quantity_example = QuantityExample(quantity=quantity_to_insert)
     session.add(quantity_example)
     session.commit()
-    result = session.query(QuantityExample).order_by(QuantityExample.id.desc()).first()
-    assert result.quantity == quantity_to_insert
-    assert result.quantity.magnitude == quantity_to_insert.magnitude
 
-
-@given(scalar_quantity=floats(allow_infinity=False, allow_nan=False))
-@pytest.mark.parametrize('quantity_type', [ureg.meter, ureg.millimeter, ureg.kilometer])
-@settings(max_examples=5)
-def test_should_insert_quantity_floats(engine, scalar_quantity, quantity_type):
-    session = Session(bind=engine)
-    quantity_to_insert = scalar_quantity * quantity_type
-    quantity_example = QuantityExample(quantity=quantity_to_insert)
-    session.add(quantity_example)
-    session.commit()
-    result = session.query(QuantityExample).order_by(QuantityExample.id.desc()).first()
+    result = session.query(QuantityExample).first()
     assert result.quantity == quantity_to_insert
     assert result.quantity.magnitude == quantity_to_insert.magnitude
