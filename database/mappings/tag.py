ea875c8ef10d93e03786670bc54ede29e4def094
JevinJ <jevinjones@openmailbox.org>
2020-01-13 01:21:06 -0800

    Add AbstractFungusTagMapping.

--- database/mappings/tag.py
+++ database/mappings/tag.py
@@ -18,6 +18,32 @@ class Tag(BaseModel):
         return {'polymorphic_identity': self.__name__}
 
 
+class AbstractFungusTagMapping(BaseModel, HasReportConsensus):
+    __abstract__ = True
+
+    @declared_attr
+    def fungus_id(self):
+        return Column(Integer, ForeignKey('fungus.id'), primary_key=True)
+
+    @declared_attr
+    def tag_id(self):
+        raise NotImplementedError
+
+    @staticmethod
+    def new_mapping(class_name, fungus_id_column: Column = None):
+        """
+        :param class_name: The name of the class being created, must be identical to
+         the name of the variable which this is being assigned to.
+        :param fungus_id_column: The column containing the fungus id of the 'left' table,
+         the table being mapped to the tags.
+        :return: Return a new derived class.
+        """
+        if fungus_id_column is None:
+            return type(class_name, (FungusTagMapping,), {})
+        return type(class_name, (FungusTagMapping,),
+                    {'fungus_id': Column(Integer, ForeignKey(fungus_id_column), primary_key=True)})
+
+
 class FungusTagMapping(BaseModel, HasReportConsensus):
     __abstract__ = True
 
