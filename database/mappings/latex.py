7c010b5b5a9858b437c70f8186566bf1c5766cba
JevinJ <jevinjones@openmailbox.org>
2020-02-14 18:12:29 -0800

    Remove FungusLatexInitialColor, FungusLatexOxidizedColor, FungusLatexStainColor. Don't worry about time related color changes.

--- database/mappings/latex.py
+++ database/mappings/latex.py
@@ -1,15 +1,7 @@
-from database.db_base import BaseModel
 from database.mappings.color import FungusColorMapping
-from sqlalchemy import Column, Integer, ForeignKey
 
 
 class FungusLatexColor(FungusColorMapping):
     """Some mushrooms, namely lactarius, exude 'milk' or latex like
      fluid when damaged or cut."""
     pass
-
-FungusLatexInitialColor = FungusColorMapping.new_mapping('FungusLatexInitialColor', fungus_id_column=Latex.fungus_id)
-
-FungusLatexOxidizedColor = FungusColorMapping.new_mapping('FungusLatexStainColor', fungus_id_column=Latex.fungus_id)
-
-FungusLatexStainColor = FungusColorMapping.new_mapping('FungusLatexStainColor', fungus_id_column=Latex.fungus_id)
