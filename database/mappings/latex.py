5a14331f370a65485f28f9a2dea7e2b154b46cc9
JevinJ <jevinjones@openmailbox.org>
2020-02-14 18:11:48 -0800

    Rename Latex->FungusLatexColor and make it a mapping.

--- database/mappings/latex.py
+++ database/mappings/latex.py
@@ -3,11 +3,10 @@ from database.mappings.color import FungusColorMapping
 from sqlalchemy import Column, Integer, ForeignKey
 
 
-class Latex(BaseModel):
+class FungusLatexColor(FungusColorMapping):
     """Some mushrooms, namely lactarius, exude 'milk' or latex like
      fluid when damaged or cut."""
-    fungus_id = Column(Integer, ForeignKey('fungus.id'), primary_key=True)
-
+    pass
 
 FungusLatexInitialColor = FungusColorMapping.new_mapping('FungusLatexInitialColor', fungus_id_column=Latex.fungus_id)
 
