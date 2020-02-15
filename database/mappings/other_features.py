6e74521fbe5ae96eb066b9e55333269b44a1bd2f
JevinJ <jevinjones@openmailbox.org>
2020-02-14 19:00:52 -0800

    Reorder some classes for clarity.

--- database/mappings/other_features.py
+++ database/mappings/other_features.py
@@ -4,8 +4,8 @@ from database.mappings.color import FungusColorMapping
 from sqlalchemy import Column, ForeignKey, Integer
 
 
-FungusOdor = FungusTagMapping.new_mapping('FungusOdor', fungus_id_column='fungus.id')
 class Odor(Tag): pass
+FungusOdor = FungusTagMapping.new_mapping('FungusOdor', fungus_id_column='fungus.id')
 
 
 class FungusLatexColor(FungusColorMapping):
@@ -18,12 +18,12 @@ class Bioluminescence(BaseModel):
     fungus_id = Column(Integer, ForeignKey('fungus.id'), primary_key=True)
 
 
-FungusEdibilityType = FungusTagMapping.new_mapping('FungusEdibilityType')
 class Edibility(Tag): pass
+FungusEdibilityType = FungusTagMapping.new_mapping('FungusEdibilityType')
 
 
-FungusTaste = FungusTagMapping.new_mapping('FungusTaste', fungus_id_column='fungus.id')
 class Taste(Tag): pass
+FungusTaste = FungusTagMapping.new_mapping('FungusTaste', fungus_id_column='fungus.id')
 
 
 class Texture(Tag): pass
